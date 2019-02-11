5aa1d55957f3270909f06177e85b5b7d3db64d7e
Gerald Bryant <hakeem1106@gmail.com>
2019-02-11 21:17:25 +0000

    set up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BackProp from './modules/backProp';
 
+ReactDOM.render(
+
+<BackProp />,
+
+document.getElementById('root')
+);
---
+++ src/modules/backProp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class BackProp extends React.Component{
+    render(){
+        return(
+            <div>
+        <form>
+        <input></input>
+        <button></button>
+        </form>
+        </div>
+        )}
+}
+export default BackProp
