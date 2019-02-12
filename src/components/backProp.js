e4b67191f94dea1304089e63a06117e788d7fb48
Gerald Bryant <hakeem1106@gmail.com>
2019-02-12 15:47:29 +0000

    updating dep

--- src/components/backProp.js
+++ src/components/backProp.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import '../components/backprop.css';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 
 
 class BackProp extends React.Component{
     render(){
         return(
+            <CSSTransitionGroup
+            transitionName="shaded"
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+            transitionEnter={false}
+            transitionLeave={false}
+            >
             <div className ="BackProp">
         <input className="TakeIt"></input>
         </div>
+        </CSSTransitionGroup>
         )}
 }
 export default BackProp
