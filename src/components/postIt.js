aa013dd8eb37dff7b13456dc4bbdb82992d680f6
Gerald Bryant <hakeem1106@gmail.com>
2019-02-17 02:40:08 +0000

    tweaking

--- src/components/postIt.js
+++ src/components/postIt.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export default class PostIt extends React.Component{
-    const square =() =>{
-        
+    square = () =>{
+
+       let y = false;
+
+       while(y !== true){
+
+
+           console.log("this is gonna work");
+       }
+
+
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BackProp from './components/backProp';
 import './components/backprop.css';
+import PostIt from './components/postIt';
 
 ReactDOM.render(
 
 <BackProp />,
+
 
 document.getElementById('root')
 );
